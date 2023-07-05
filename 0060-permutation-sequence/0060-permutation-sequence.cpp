930100b316e1913b52b195fa320058187a3e2b12
Shambhabi Dhar <109088874+ShambhabiDhar@users.noreply.github.com>
2023-07-05 18:59:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (46.93%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    void plist(int n, std::string& s, std::vector<int>& nums, std::vector<std::string>& res, std::vector<bool>& freq) {
-        if (s.size() == n) {
-            res.push_back(s);
-            return;
+    string getPermutation(int n, int k) {
+        string ans="";
+        int fact=1;
+        vector<int> nums;
+        for (int i=1; i<n; i++){
+            fact=fact*i;
+            nums.push_back(i); // n*(n-1)!
         }
-        for (int i = 0; i < n; i++) {
-            if (!freq[i]) {
-                s.push_back('0' + nums[i]);
-                freq[i] = true;
-                plist(n, s, nums, res, freq);
-                freq[i] = false;
-                s.pop_back();
+        nums.push_back(n);
+        k=k-1;
+        while(true){
+            ans=ans+to_string(nums[k / fact]);
+            nums.erase(nums.begin() + k / fact);
+            if (nums.size()==0){
+                break;
             }
+            k=k%fact;
+            fact=fact/nums.size();
         }
-    }
-
-    std::string getPermutation(int n, int k) {
-        std::vector<int> nums(n);
-        std::iota(nums.begin(), nums.end(), 1);
-        std::vector<bool> freq(n, false);
-        std::vector<std::string> res;
-        std::string s;
-        plist(n, s, nums, res, freq);
-        return res[k - 1];
+        return ans;        
     }
 };
